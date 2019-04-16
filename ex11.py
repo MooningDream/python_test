fb3abcf894d1e0adc8d1b0603598fe516fde8ac5
MooningDream <mooningdream@gmail.com>
2019-04-16 20:44:17 +0800

    new

--- ex11.py
+++ ex11.py
@@ -1,9 +1,9 @@
 print("How old are you?", end=' ')
-age = input()
+age = int(input())
 print("How tall are you?", end=' ')
 height = input()
 print("How much do you weight?", end=' ')
 weight = input()
 
-print("You are {} years old, {} cms tall and {} kgs weight.".format(age, height, weight))
-print(f"You are {age} years old, {height} cms tall and {weight} kgs weight.") # 注意这个f的作用
+print("You are {} years old, {} cms tall and {} kgs weight.".format(age+1, height, weight))
+print(f"You are {age} years old, {height} cms tall and {weight} kgs weight.") # 注意这个f
