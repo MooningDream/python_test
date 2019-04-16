027a242c73aceb9dab732dab266903437279dfef
MooningDream <mooningdream@gmail.com>
2019-04-16 22:33:18 +0800

    new

--- ex13.py
+++ ex13.py
@@ -1,1 +1,11 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, thrid = argv
 
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your thrid variables is:", thrid)
+
+script_1 = input("The script called? ")
+print(script == script_1)
