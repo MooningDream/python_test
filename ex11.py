be3e70764e1cac70aff16708c85117cc81492d08
MooningDream <mooningdream@gmail.com>
2019-04-16 12:51:30 +0800

    new

--- ex11.py
+++ ex11.py
@@ -6,4 +6,4 @@ print("How much do you weight?", end=' ')
 weight = input()
 
 print("You are {} years old, {} cms tall and {} kgs weight.".format(age, height, weight))
-print(f"You are {age} years old, {height} cms tall and {weight} kgs weight.")
+print(f"You are {age} years old, {height} cms tall and {weight} kgs weight.") # 注意这个f的作用
