42fea8b665d9d4b5492099fd1bad086aed78b0d2
MooningDream <mooningdream@gmail.com>
2019-04-21 16:03:07 +0800

    new

--- ex12.py
+++ ex12.py
@@ -2,4 +2,4 @@ year = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
-print(f"You are {year} years old, {height} cms tall and {weight} kgs heavy.")-
+print(f"You are {year} years old, {height} cms tall and {weight} kgs heavy.")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-
-cat_voice = "\a\a\a\a\a\a\a\a\a"
-print("Here is a cat." + cat_voice)
+print('I "said" do not touch this.')
